4a80ae4cdbfbc24a55baa02b1bfb1016a3851ba1
Julian Andres Garnica Alvarez <julian.garnica@lilipink.local>
2022-11-09 17:23:43 -0500

    configuracion vuetify 2, vue 2 y laravel 8

---
+++ resources/js/components/WelcomeComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Welcome</h1>
+        <v-btn color="primary">Button</v-btn>
+        <v-alert dismissible type="success">Alerta</v-alert>
+    </div>
+</template>
+<script>
+    export default {
+    
+    }
+</script>
